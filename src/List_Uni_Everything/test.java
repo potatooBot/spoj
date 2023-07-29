17c1ae59f04826a17867149f877a1734c9e7c12e
potatooBot <kumarkeshav825@gmail.com>
2023-07-29 23:05:02 +0530

    Segmented Sieve

---
+++ src/List_Uni_Everything/test.java
@@ -0,0 +1,22 @@
+package List_Uni_Everything;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<>();
+//        int n=sc.nextInt();
+        for (int i = 0; i <100000 ; i++) {
+
+int val=sc.nextInt();
+if(val==42) break;
+            System.out.println(val);
+
+        }
+
+
+
+    }
+}
---
+++ src/Prime_Generator/Segmented_Sieve.java
@@ -0,0 +1,76 @@
+package Prime_Generator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Segmented_Sieve {
+    public static void simpleSieve(boolean prime[]){
+
+        Arrays.fill(prime,true);
+prime[1]=false;
+        for (int p = 2; p*p <=1000000 ; p++) {
+            if(prime[p]==true) {
+                for (int i = p * p; i <=1000000; i = i + p) {
+                    prime[i] = false;
+                }
+            }
+        }
+    }
+    public static ArrayList<Integer> getPrime(int n,boolean prime[] ){
+        ArrayList<Integer> ds=new ArrayList<>();
+        for (int i = 2; i <=n ; i++) {
+            if(prime[i]==true) {
+                ds.add(i);
+            }
+        }
+        return ds;
+    }
+    public static void segmentedSieve(int left,int right,ArrayList<Integer> primeValues){
+        int dummy[]=new int[right-left +1];
+dummy[1]=0;
+dummy[0]=0;
+        Arrays.fill(dummy,1);
+
+
+        for ( int ele :primeValues) {
+
+
+            int firstMultiple =(left/ele)*ele;
+
+            if(firstMultiple<left){
+                firstMultiple=firstMultiple+ele;
+            }
+
+            for (int i = Math.max(firstMultiple,ele*ele); i <=right ; i=i+ele) {
+dummy[i-left]=0;
+            }
+        }
+
+        for (int i = left; i <=right ; i++) {
+            if(dummy[i-left]==1){
+                if(i==1) continue;
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int t=sc.nextInt();
+        boolean prime[]=new boolean[1000001];
+    simpleSieve(prime);
+        for (int i = 0; i <t ; i++) {
+            int left=sc.nextInt();
+            int right=sc.nextInt();
+            ArrayList<Integer> primeValue=getPrime((int)Math.sqrt(right),prime);
+            segmentedSieve(left,right,primeValue);
+            primeValue.clear();
+        }
+
+
+
+    }
+}
